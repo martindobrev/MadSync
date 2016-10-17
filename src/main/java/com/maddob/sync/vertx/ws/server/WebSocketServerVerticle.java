a9d0a095b674f471d293ab9d9bef14fdd470752e
martindobrev <martin@dobrev.eu.com>
2016-10-17 18:48:06 +0300

    index.html added with websocket support
    Simple echo server handler added for websockets

--- src/main/java/com/maddob/sync/vertx/ws/server/WebSocketServerVerticle.java
+++ src/main/java/com/maddob/sync/vertx/ws/server/WebSocketServerVerticle.java
@@ -2,7 +2,13 @@ package com.maddob.sync.vertx.ws.server;
 
 import io.vertx.core.*;
 import io.vertx.core.http.*;
+import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import io.vertx.ext.web.handler.sockjs.SockJSHandlerOptions;
+
+import java.util.logging.Logger;
 
 /**
  * WebSocket server verticle for
@@ -23,15 +29,20 @@ public class WebSocketServerVerticle extends AbstractVerticle {
 
         Router router = Router.router(getVertx());
 
-        router.route().handler(routingContext -> {
+//        router.route("/").handler(routingContext -> {
+//
+//            HttpServerResponse response = routingContext.response();
+//            response.putHeader("content-type", "text/html");
+//            response.end("Hello World from vertx!!!");
+//        });
 
-            HttpServerResponse response = routingContext.response();
-            response.putHeader("content-type", "text/plain");
-            response.end("Hello WOrld from vertx!!!");
-        });
+        SockJSHandlerOptions options = new SockJSHandlerOptions().setHeartbeatInterval(2000);
+        SockJSHandler sockJsHandler = SockJSHandler.create(getVertx(), options);
+        sockJsHandler.socketHandler(sockJSSocket -> sockJSSocket.handler(sockJSSocket::write));
 
+        router.route("/myapp/*").handler(sockJsHandler);
+        router.route("/*").handler(StaticHandler.create());
         server.requestHandler(router::accept).listen(8080);
-
     }
 
     @Override
